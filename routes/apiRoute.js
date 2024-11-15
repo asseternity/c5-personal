2c7b3485004d98b11e1a7ccd1097701cbb473c8b
asseternity <nakupovasset@gmail.com>
2024-11-15 23:46:58 +0400

    fix: rename allMessages route for CORS

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -17,6 +17,6 @@ apiRoute.post("/sign-up", apiController.postSignUp);
 apiRoute.post("/log-in", apiController.postLogIn);
 apiRoute.get("/message", apiController.getMessage);
 apiRoute.post("/message", apiController.postMessage);
-apiRoute.post("/allMessages/", apiController.getAllUserMessages);
+apiRoute.post("/allMessages", apiController.getAllUserMessages);
 
 module.exports = apiRoute;
